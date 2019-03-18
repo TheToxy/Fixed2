9e786a6dd7af167665681c1ef1d497e3ba85215a
Ondrej Kraus <klabzub@seznam.cz>
2019-03-18 17:27:44 +0100

    Last
    Com

---
+++ FixedPoint.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("BestLibrary.UnitTests")]
+namespace Cuni.Arithmetics.FixedPoint
+{
+    public interface IArithmetics
+    {
+        int Multilpy(Q32 a);
+        int Divide(Q32 a);
+        int Add(Q32 a);
+        int Subtract(Q32 a);
+        double DoubleValue { get; }
+
+        Q32 WithValue(int a);
+        Q32 WithRawValue(int a);
+
+
+        //int GetFractionLength { get; }
+    }
+
+    public struct Fixed<T> where T : IArithmetics, new()
+    {
+        internal Q32 FixedPoint { get; }
+
+        public override string ToString()
+        {
+            return ((double)this).ToString();
+        }
+
+        internal Fixed(Q32 fixedPoint)
+        {
+            FixedPoint = fixedPoint;
+        }
+        public Fixed(int number) : this(new T().WithValue(number)) { }
+
+        public Fixed<T> Multiply(Fixed<T> other)
+        {
+            var newNumber = FixedPoint.Multilpy(other.FixedPoint);
+            var a = new Fixed<T>(new T().WithRawValue(newNumber));            
+            return a;
+        }
+
+        public Fixed<T> Add(Fixed<T> other)
+        {
+            int number = FixedPoint.Add(other.FixedPoint);
+            var a = new Fixed<T>(new T().WithRawValue(number));
+            return a;
+        }
+
+        public Fixed<T> Subtract(Fixed<T> other)
+        {
+            int number = FixedPoint.Subtract(other.FixedPoint);
+            var a = new Fixed<T>(new T().WithRawValue(number));
+            return a;
+        }
+
+        public Fixed<T> Divide(Fixed<T> other)
+        {
+            int number = FixedPoint.Divide(other.FixedPoint);
+            var a = new Fixed<T>(new T().WithRawValue(number));
+            return a;
+        }
+
+        public static implicit operator double(Fixed<T> number)
+        {
+            return number.FixedPoint.DoubleValue;
+        }
+    }
+
+    public abstract class Q32
+    {
+        public static int IntegerLength = 32;
+        public static int FractionLength;
+
+        /// <summary>
+        /// To make it easy when using arithmetics
+        /// </summary>
+        internal long RawLongValue { get; }
+   
+        public double DoubleValue
+        {
+            get
+            {
+                return RawLongValue / (double)(1 << FractionLength);
+            }
+        }
+        
+        protected Q32(int number)
+        {
+            RawLongValue = number;
+        }
+
+        public int Multilpy(Q32 other)
+        {
+            bool isResultNegative = (this.RawLongValue < 0) ^ (other.RawLongValue < 0);
+            long num = Math.Abs(RawLongValue) * Math.Abs(other.RawLongValue);
+            int result = (int)(num >> FractionLength);
+            if (isResultNegative)
+            {
+                result *= -1;
+            }
+            return result;            
+        }
+
+        public int Divide(Q32 other)
+        {
+            bool isResultNegative = (this.RawLongValue < 0) ^ (other.RawLongValue < 0);
+            long num = (Math.Abs(RawLongValue) << FractionLength) / Math.Abs(other.RawLongValue);
+            int result = (int)num;
+            if (isResultNegative)
+            {
+                result *= -1;
+            }
+            return result;
+        }
+
+        public int Add(Q32 other)
+        {
+            long num = this.RawLongValue + other.RawLongValue;                           
+            return (int)num;
+        }
+
+        public int Subtract(Q32 other)
+        {
+            long num = this.RawLongValue - other.RawLongValue;                              
+            return (int)num;
+        }
+    }
+
+    public sealed class Q24_8 : Q32, IArithmetics
+    {
+        static Q24_8()
+        {
+            FractionLength = 8;
+        }
+
+        private Q24_8(int number) : base(number) { }
+
+        public Q24_8() : base(0) { }
+
+        public Q32 WithValue(int number)
+        {
+            return new Q24_8(number << FractionLength);
+        }
+
+        public Q32 WithRawValue(int number)
+        {
+            return new Q24_8(number);
+        }
+
+        //public static implicit operator Q24_8(int number)
+        //{
+        //    return new Q24_8(number);
+        //}
+
+    }
+
+    public sealed class Q16_16 : Q32, IArithmetics
+    {
+        static Q16_16()
+        {
+            FractionLength = 16;
+        }
+
+        public Q16_16(int number) : base(number) { }
+
+        public Q16_16() : base(0) { }
+
+        public Q32 WithValue(int number)
+        {
+            return new Q16_16(number << FractionLength);
+        }
+        public Q32 WithRawValue(int number)
+        {
+            return new Q16_16(number);
+        }
+    }
+
+    public sealed class Q8_24 : Q32, IArithmetics
+    {
+        static Q8_24()
+        {
+            FractionLength = 24;
+        }
+
+        public Q8_24(int number) : base(number) { }
+
+        public Q8_24() : base(0) { }
+
+        public Q32 WithValue(int number)
+        {
+            return new Q8_24(number << FractionLength);
+        }
+        public Q32 WithRawValue(int number)
+        {
+            return new Q8_24(number);
+        }
+    }
+}
+    
---
+++ Q32Tests.cs
@@ -0,0 +1,228 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Cuni.Arithmetics.FixedPoint;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cuni.Arithmetics.FixedPoint.Tests
+{
+    [TestClass()]
+    public class FixedTests
+    {
+        [TestMethod()]
+        public void ToStringTest()
+        {
+            int a = 5;
+            var aFixed = new Fixed<Q24_8>(a);
+            Assert.AreEqual(aFixed.ToString(), a.ToString());
+            int b = -45;
+            var bFixed = new Fixed<Q24_8>(b);
+
+            var resultFixed = aFixed.Divide(bFixed);
+            Assert.AreEqual(Math.Round(resultFixed, Q32Tests.roundDeximalCount).ToString(),
+                Math.Round((a / (double)b), Q32Tests.roundDeximalCount).ToString());
+
+        }
+    }
+    [TestClass()]
+    public class Q32Tests
+    {
+        public static int roundDeximalCount = 1;
+
+        public Fixed<T> Add<T>(int a, int b) where T : IArithmetics, new()
+        {
+            var aFixed = new Fixed<T>(a);
+            var bFixed = new Fixed<T>(b);
+            var resultFixed = aFixed.Add(bFixed);
+            Assert.AreEqual(Math.Round(b + (double)a, roundDeximalCount),
+                Math.Round(resultFixed, roundDeximalCount));
+            return resultFixed;
+        }
+
+        public Fixed<T> Subtract<T>(int a, int b) where T : IArithmetics, new()
+        {
+            var aFixed = new Fixed<T>(a);
+            var bFixed = new Fixed<T>(b);
+            var resultFixed = aFixed.Subtract(bFixed);
+            Assert.AreEqual(Math.Round(a - (double)b, roundDeximalCount),
+                Math.Round(resultFixed, roundDeximalCount));
+            return resultFixed;
+        }
+        public Fixed<T> Multiply<T>(int a, int b) where T : IArithmetics, new()
+        {
+            var aFixed = new Fixed<T>(a);
+            var bFixed = new Fixed<T>(b);
+            var resultFixed = aFixed.Multiply(bFixed);
+            Assert.AreEqual(Math.Round(a * (double)b, roundDeximalCount), 
+                Math.Round(resultFixed, roundDeximalCount));
+            return resultFixed;
+        }
+        public Fixed<T> Divide<T>(int a, int b) where T : IArithmetics, new()
+        {
+            var aFixed = new Fixed<T>(a);
+            var bFixed = new Fixed<T>(b);
+            var resultFixed = aFixed.Divide(bFixed);
+            Assert.AreEqual(Math.Round(a / (double)b, roundDeximalCount), 
+                Math.Round(resultFixed, roundDeximalCount));
+            return resultFixed;
+        }
+
+        [TestMethod()]
+        public void SubtractingQ24_08()
+        {
+            int a = 5;
+            int b = 7;
+            var aFixed = new Fixed<Q24_8>(a);
+            var bFixed = new Fixed<Q24_8>(b);
+            var resultFixed = aFixed.Subtract(bFixed);
+            Assert.AreEqual(a - (double)b, resultFixed);
+        }
+
+        [TestMethod()]
+        public void DivisionMinusTests()
+        {
+            Divide<Q24_8>(5, -7);
+            Divide<Q16_16>(5, -7);
+            Divide<Q8_24>(5, -7);
+
+            Divide<Q24_8>(-5, -7);
+            Divide<Q16_16>(-5, -7);
+            Divide<Q8_24>(-5, -7);
+
+        }
+
+        [TestMethod()]
+        public void DivideByLowerMinusFixedQ24_08()
+        {
+            int a = 5;
+            int b = 7;
+            var aFixed = new Fixed<Q16_16>(a);
+            var bFixed = new Fixed<Q16_16>(b);
+            var subtractResult = aFixed.Subtract(bFixed);
+            // 5 - 7 
+            Assert.AreEqual(a - (double)b, 
+                Math.Round((double)subtractResult, roundDeximalCount));
+
+
+            // 5/-2
+            var divideResult = aFixed.Divide(subtractResult);
+            Assert.AreEqual(Math.Round((double)divideResult, roundDeximalCount), 
+                Math.Round((double)a/ -2, roundDeximalCount));
+        }
+
+        [TestMethod()]
+        public void DivideByLowerPlusFixedQ16_16()
+        {
+            int a = 5;
+            int b = 7;
+            var aFixed = new Fixed<Q16_16>(a);
+            var bFixed = new Fixed<Q16_16>(b);
+
+            // 5/7
+            var divideResult = aFixed.Divide(bFixed);
+            Assert.AreEqual(Math.Round((double)divideResult, roundDeximalCount), 
+                Math.Round((double)a / (double)b, roundDeximalCount));
+        }
+
+        [TestMethod()]
+        public void DivideByLowerMinusFixedQ8_24()
+        {
+            int a = 5;
+            int b = 7;
+            var aFixed = new Fixed<Q8_24>(a);
+            var bFixed = new Fixed<Q8_24>(b);
+            // 5 - 7 = -2 
+            var subtractResult = aFixed.Subtract(bFixed);
+            // 5/ -2 = -2.5
+            var divideResult = aFixed.Divide(subtractResult);
+            Assert.AreEqual(Math.Round( a / subtractResult, roundDeximalCount),
+                Math.Round(divideResult, roundDeximalCount));
+        }
+
+        [TestMethod()]
+        public void DivideSameNumbersQ8_24()
+        {
+            int a = 7;
+            int b = 7;
+            var aFixed = new Fixed<Q8_24>(a);
+            var bFixed = new Fixed<Q8_24>(b);
+            
+            // 5/ -2 = -2.5
+            var divideResult = aFixed.Divide(bFixed);
+            Assert.AreEqual(1, Math.Round(divideResult, roundDeximalCount));
+        }
+        [TestMethod()]
+        public void DivideSameNumbersMinusQ8_24()
+        {
+            int a = 7;
+            
+            var aFixed = new Fixed<Q8_24>(a);
+            var zeroFixed = new Fixed<Q8_24>(0);
+
+            var negAFixed = zeroFixed.Subtract(aFixed);
+            // 0 - 7= -7
+            var divideResult = aFixed.Divide(negAFixed);
+            Assert.AreEqual(-1, Math.Round(divideResult, roundDeximalCount));
+        }
+
+        [TestMethod()]
+        public void DivideSameNumberQ8_24()
+        {
+            Divide<Q8_24>(7, -7);          
+            Divide<Q8_24>(-7, -7);          
+            Divide<Q8_24>(7, 7);          
+            Divide<Q8_24>(-7, 7);            
+        }
+
+        [TestMethod()]
+        public void DivideSameNumbersMinusMinusQ8_24()
+        {
+            Divide<Q8_24>(-7, 7);
+        }
+
+        [TestMethod()]
+        public void MultByZeroQ8_24()
+        {
+            int a = 120;
+            var zero = Add<Q24_8>(-5, 5);
+            var aFixed = new Fixed<Q24_8>(a);
+            var multResult = zero.Multiply(aFixed);
+            Assert.AreEqual(0, Math.Round(multResult, roundDeximalCount));
+        }
+
+        [TestMethod()]
+        public void MultTwoMinusesQ8_24()
+        {            
+            var a = Multiply<Q24_8>(-12, -5);
+            var b = Multiply<Q24_8>(-12, 5);
+            var c = Multiply<Q24_8>(12, -5);
+        }
+
+        [TestMethod()]
+        public void MultBigQ8_24()
+        {
+            // Too big numbers
+            int a = 7;
+            int b = 50;
+            var aFixed = new Fixed<Q8_24>(a);
+            var bFixed = new Fixed<Q8_24>(b);
+
+            var negAFixed = bFixed.Multiply(aFixed);
+            //Assert.AreEqual(a * b, Math.Round(negAFixed, roundDeximalCount));
+            Assert.Inconclusive();
+        }
+
+        [TestMethod()]
+        public void MultSmallNumbersQ8_24()
+        {
+            var xFixed = Divide<Q24_8>(15, 10);
+            var aFixed = Divide<Q24_8>(1, 15);
+            var bFixed = Divide<Q24_8>(-2, 5);
+            var resultFixed = aFixed.Multiply(bFixed);
+            Assert.AreEqual(Math.Round(aFixed * bFixed, roundDeximalCount), 
+                Math.Round(resultFixed, roundDeximalCount));
+        }
+    }
+}
